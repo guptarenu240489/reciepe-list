dfa6c83f4bc9f56e3d5dcba06516c6d1ef71a0ad
guptarenu240489 <guptarenu24@gmail.com>
2019-09-19 16:10:36 +0530

    adding basic component layouts

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Noodles', 'Topped with chilli sauce','https://upload.wikimedia.org/wikipedia/commons/4/46/Shaxian_noodles_%2820170119120759%29.jpg')
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
